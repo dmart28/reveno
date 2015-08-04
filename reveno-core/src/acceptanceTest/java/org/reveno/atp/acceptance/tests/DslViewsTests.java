b65ecb86c0508a49c0289103ed744ee3335855c1
Artem Dmitriev <art.dm.ser@gmail.com>
2015-08-04 08:33:46 +0300

    Not compiling prototype, see also https://app.asana.com/0/30352674872438/44115831814542 for single dynamic link using Supplier<View>.

--- reveno-core/src/acceptanceTest/java/org/reveno/atp/acceptance/tests/DslViewsTests.java
+++ reveno-core/src/acceptanceTest/java/org/reveno/atp/acceptance/tests/DslViewsTests.java
@@ -1,8 +1,8 @@
 package org.reveno.atp.acceptance.tests;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,7 +10,40 @@ import org.reveno.atp.api.Configuration.ModelType;
 import org.reveno.atp.api.commands.dynamic.DynamicCommand;
 import org.reveno.atp.utils.MapUtils;
 
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+import it.unimi.dsi.fastutil.longs.LongList;
+
 public class DslViewsTests extends RevenoBaseTest {
+	
+	@Test
+	public void onDemandViewCreationTest() throws Exception {
+		TestRevenoEngine reveno = new TestRevenoEngine(tempDir);
+		reveno.config().modelType(ModelType.MUTABLE);
+		
+		DynamicCommand createPage = reveno.domain().transaction("createPage", (tx, ctx) -> {
+		    ctx.repository().store(tx.id(), new Page(tx.id(), tx.arg()));
+		}).uniqueIdFor(Page.class).command();
+		DynamicCommand createLink = reveno.domain().transaction("createLink", (tx, ctx) -> {
+			if (tx.opArg("page").isPresent()) {
+		    	ctx.repository().get(Page.class, tx.arg("page")).ifPresent(p -> p.linksIds.add(tx.arg("page")));
+		    }
+		    ctx.repository().store(tx.id(), new Link(tx.id(), tx.arg("name"), tx.arg("url")));
+		}).uniqueIdFor(Link.class).command();
+		
+		reveno.domain().viewMapper(Link.class, LinkView.class, (e,o,r) -> new LinkView(e.id, e.name, e.url));
+		reveno.domain().viewMapper(Page.class, PageView.class, (e,o,r) -> 
+			new PageView(e.id, e.name, r.dynamic(e.linksIds.stream(), LinkView.class, Collectors.toList()));
+		
+		reveno.startup();
+		
+		long pageId = reveno.executeSync(createPage, MapUtils.map("name", "Open Source"));
+		long linkId = reveno.executeSync(createLink, MapUtils.map("name", "reveno", "url", "http://reveno.org", "page", pageId));
+		
+		Assert.assertTrue(reveno.query().find(PageView.class, pageId).isPresent());
+		PageView page = reveno.query().find(PageView.class, pageId).get();
+		Assert.assertEquals(page.links.size(), 1);
+		Assert.assertEquals(page.links.get(0).id, linkId);
+	}
 	
 	@Test
 	public void shortestSyntax() throws Exception {
@@ -27,7 +60,7 @@ public class DslViewsTests extends RevenoBaseTest {
 		
 		reveno.config().modelType(ModelType.MUTABLE);
 		DynamicCommand createPage = f.apply(reveno);
-		reveno.domain().viewMapper(Page.class, PageView.class, (e,o,r) -> new PageView(e.name, null));
+		reveno.domain().viewMapper(Page.class, PageView.class, (e,o,r) -> new PageView(e.id, e.name, null));
 		
 		reveno.startup();
 		
@@ -41,7 +74,7 @@ public class DslViewsTests extends RevenoBaseTest {
 		reveno = new TestRevenoEngine(tempDir);
 		reveno.config().modelType(ModelType.MUTABLE);
 		createPage = f.apply(reveno);
-		reveno.domain().viewMapper(Page.class, PageView.class, (e,o,r) -> new PageView(e.name, null));
+		reveno.domain().viewMapper(Page.class, PageView.class, (e,o,r) -> new PageView(e.id, e.name, null));
 		
 		reveno.startup();
 		
@@ -68,7 +101,7 @@ public class DslViewsTests extends RevenoBaseTest {
 	public static class Page {
 		public long id;
 		public String name;
-		public List<Integer> linksIds = new ArrayList<>();
+		public LongList linksIds = new LongArrayList();
 		
 		public Page(long id, String name) {
 			this.id = id;
@@ -80,23 +113,33 @@ public class DslViewsTests extends RevenoBaseTest {
 		public long id;
 		public String name;
 		public String url;
+		
+		public Link(long id, String name, String url) {
+			this.id = id;
+			this.name = name;
+			this.url = url;
+		}
 	}
 	
 	public static class PageView {
+		public final long id;
 		public final String name;
-		public final List<LinkView> views;
+		public final List<LinkView> links;
 		
-		public PageView(String name, List<LinkView> views) {
+		public PageView(long id, String name, List<LinkView> links) {
+			this.id = id;
 			this.name = name;
-			this.views = views;
+			this.links = links;
 		}
 	}
 	
 	public static class LinkView {
+		public final long id;
 		public final String name;
 		public final String url;
 		
-		public LinkView(String name, String url) {
+		public LinkView(long id, String name, String url) {
+			this.id = id;
 			this.name = name;
 			this.url = url;
 		}
