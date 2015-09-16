e730ff534d4ba96472d31b30ba8aa3dee97cdc96
Artem Dmitriev <art.dm.ser@gmail.com>
2015-09-16 06:03:28 +0300

    move cluster api to separate project #32251622346055

--- reveno-cluster/src/main/java/org/reveno/atp/clustering/api/message/Marshaller.java
+++ reveno-cluster/src/main/java/org/reveno/atp/clustering/api/message/Marshaller.java
@@ -1,4 +1,4 @@
-/** 
+/**
  *  Copyright (c) 2015 The original author or authors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -6,7 +6,6 @@
  *  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,8 +19,8 @@ import org.reveno.atp.core.api.channel.Buffer;
 
 public interface Marshaller {
 
-	Buffer marshall(Message message);
-	
-	Message unmarshall(Buffer buffer);
-	
+    Buffer marshall(Message message);
+
+    Message unmarshall(Buffer buffer);
+
 }
--- reveno-cluster/src/main/java/org/reveno/atp/clustering/api/message/Message.java
+++ reveno-cluster/src/main/java/org/reveno/atp/clustering/api/message/Message.java
@@ -1,4 +1,4 @@
-/** 
+/**
  *  Copyright (c) 2015 The original author or authors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -6,7 +6,6 @@
  *  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,13 +19,13 @@ import org.reveno.atp.clustering.api.NodeAddress;
 
 public abstract class Message {
 
-	private transient NodeAddress address;
-	public NodeAddress address() {
-		return address;
-	}
-	public Message address(NodeAddress address) {
-		this.address = address;
-		return this;
-	}
-	
+    private transient NodeAddress address;
+    public NodeAddress address() {
+        return address;
+    }
+    public Message address(NodeAddress address) {
+        this.address = address;
+        return this;
+    }
+
 }
